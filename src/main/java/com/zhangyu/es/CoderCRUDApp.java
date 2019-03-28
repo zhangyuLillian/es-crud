c4616e109fd50c1da11ecd039b1df9ad537e9053
zhangyu <zhangyu_1203@foxmail.com>
2019-03-28 17:17:43 +0800

    增加多条件查询

--- src/main/java/com/zhangyu/es/CoderCRUDApp.java
+++ src/main/java/com/zhangyu/es/CoderCRUDApp.java
@@ -3,11 +3,15 @@ package com.zhangyu.es;
 import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.elasticsearch.common.xcontent.XContentFactory;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
 
 import java.io.IOException;
@@ -26,13 +30,15 @@ public class CoderCRUDApp {
         try {
             client = initClient();
             //新增
-            createCoder(client);
+            //createCoder(client);
             //修改
-            updateCoder(client);
+            //updateCoder(client);
             //查询
-            getCoder(client);
+            //getCoder(client);
             //删除
-            deleteCoder(client);
+            //deleteCoder(client);
+            //复杂条件查询
+            searchCoders(client);
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
@@ -59,14 +65,15 @@ public class CoderCRUDApp {
      * @throws IOException
      */
     private static void createCoder(TransportClient client) throws IOException {
-        IndexResponse response = client.prepareIndex("company","coder","1")
+        IndexResponse response = client.prepareIndex("company","coder","4")
                                        .setSource(XContentFactory.jsonBuilder()
                                                .startObject()
-                                                    .field("name","zhao")
-                                                    .field("age",27)
+                                                    .field("name","李")
+                                                    .field("age",28)
                                                     .field("country","china")
-                                                    .field("salary",15000)
+                                                    .field("salary",17000)
                                                     .field("language","java")
+                                                    .field("join_date","2018-3-1")
                                                .endObject())
                                        .get();
         System.out.println(response.getResult());
@@ -94,6 +101,26 @@ public class CoderCRUDApp {
     private static void getCoder(TransportClient client) {
         GetResponse response = client.prepareGet("company","coder","1").get();
         System.out.println(response.getSourceAsString());
+    }
+
+    /**
+     * 需求：
+     * 1、语言是java
+     * 2、薪水在15k-20k
+     * 3、分页查询 查找第一页
+     * @param client
+     */
+    private static void searchCoders(TransportClient client) {
+        SearchResponse response = client.prepareSearch("company")
+                                        .setTypes("coder")
+                                        .setQuery(QueryBuilders.matchQuery("language","java"))
+                                        .setPostFilter(QueryBuilders.rangeQuery("salary").from(13000).to(25000))
+                                        .setFrom(0).setSize(5)
+                                    .get();
+        SearchHit[] searchHits = response.getHits().getHits();
+        for (int i = 0;i < searchHits.length;i ++) {
+            System.out.println(searchHits[i].getSourceAsString());
+        }
     }
 
     /**
